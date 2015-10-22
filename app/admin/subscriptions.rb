25cfd38da96ac02e38707a432018d2b892ecbbdd
verny <vrivera@growthaccelerationpartners.com>
2015-10-22 07:45:31 -0600

    making subcriptions to work

--- app/admin/subscriptions.rb
+++ app/admin/subscriptions.rb
@@ -3,19 +3,19 @@ ActiveAdmin.register Subscription do
   filter :group_id
   filter :amount
   filter :created_at
-  scope :non_zero
-  scope :zero
+  #scope :non_zero
+  #scope :zero
 
-  index do
-    column :group_id
-    column :group do |s|
-      Group.find(s.group_id).name
-    end
-    column :amount
-    column :created_at
-    column :updated_at
-    actions
-  end
+#  index do
+#    column :group_id
+#    column :group do |s|
+#      Group.find(s.group_id).name
+#    end
+#    column :amount
+#    column :created_at
+#    column :updated_at
+#    actions
+#  end
 
   member_action :update, :method => :put do
     subscription = Subscription.find(params[:id])
